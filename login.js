73e69cc8e53b24964081fdc75f02139230d4edf4
guipborges <guipborges@gmail.com>
2026-01-16 21:49:03 +0000

    Update 2

--- login.js
+++ login.js
@@ -1,39 +1,26 @@
+
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.9.0/firebase-app.js';
+import { getAuth, GoogleAuthProvider, signInWithPopup, onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/10.9.0/firebase-auth.js';
+
 const firebaseConfig = {
-  apiKey: "AIzaSyC2ZwTQwTP6w-OfyH2U6jYCw_ULrqGsJu4",
-  authDomain: "planner-28848511-588c9.firebaseapp.com",
-  projectId: "planner-28848511-588c9",
-  storageBucket: "planner-28848511-588c9.appspot.com",
-  messagingSenderId: "710896904766",
-  appId: "1:710896904766:web:64d6cca17eba34c5ad7f6f"
-};
+    apiKey: "AIzaSyC2ZwTQwTP6w-OfyH2U6jYCw_ULrqGsJu4",
+    authDomain: "planner-28848511-588c9.firebaseapp.com",
+    projectId: "planner-28848511-588c9",
+    storageBucket: "planner-28848511-588c9.firebasestorage.app",
+    messagingSenderId: "710896904766",
+    appId: "1:710896904766:web:64d6cca17eba34c5ad7f6f"
+    };
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
 
-const auth = firebase.auth();
-const provider = new firebase.auth.GoogleAuthProvider();
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+        window.location.href = "planner.html";
+    }
+});
 
-const loginButton = document.getElementById('login-button');
-
-loginButton.addEventListener('click', () => {
-    auth.signInWithPopup(provider)
-        .then((result) => {
-            // This gives you a Google Access Token. You can use it to access the Google API.
-            const credential = result.credential;
-            const token = credential.accessToken;
-            // The signed-in user info.
-            const user = result.user;
-            // IdP data available using getAdditionalUserInfo(result)
-            // ...
-            window.location.href = "planner.html";
-        }).catch((error) => {
-            // Handle Errors here.
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            // The email of the user's account used.
-            const email = error.customData.email;
-            // The AuthCredential type that was used.
-            const credential = error.credential;
-            // ...
-        });
+document.getElementById('login-button').addEventListener('click', () => {
+    signInWithPopup(auth, provider).catch(error => console.error('Error during login:', error));
 });
